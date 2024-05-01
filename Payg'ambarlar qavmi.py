48f9fa71a26c13888f117c1e8e7f0f8801dcb4b7
Shokhrukh <shohruhganjayev399@gmail.com>
2024-05-01 16:23:22 +0500

    Surah and ayahs
    Here you can write the name of surah and the code will show you there are how many ayahs in it

---
+++ Payg'ambarlar qavmi.py
@@ -0,0 +1,13 @@
+"""About my family"""
+dad={'name':'Azamat', 'born year':1975, 'job':'taxi driver'}
+mum={'name':'Shakar', 'born year':1976, 'job':'doesn\'t work'}
+sister={'name':'Oynisa', 'born year':1995, 'job':'school teacher'}
+me={'name':'Shohruh', 'born year':1998, 'job':'taxi driver,\
+teacher at University, teacher at driving school,\
+junior researcher at Institute and InshaAlloh future excellent coder'}
+brother={'name':'Bekturdi', 'born year':2001, 'job':'crane driver'}
+print(f"My dad's name is {dad['name']} and he was born in {dad['born year']}.\
+ He works on and off as a {dad['job']} for about 20 years. The name of\
+ my mum is {mum['name']}, she was born in {mum['born year']} and she {mum['job']}.\
+ Coming to my sister {sister}. My brother {brother}. Finally I was born in {me['born year']}\
+. I work(ed) in several positions. Here they are: {me['job']}")
